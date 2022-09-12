a4bfd38eef93b1a82fe78a3b49481c59b87c3529
Aly Mohamed <alyabdulmottalib@gmail.com>
2022-09-12 22:31:03 +0200

    init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useRef, useState } from "react";
+import "./App.css";
+import { TruthTable } from "./TruthTable";
 function App() {
+  const [errorMsg, setErrorMsg] = useState("none");
+  const [equation, setEquation] = useState("");
+  const [validEquation, setValidEqn] = useState(false);
+  const validateEquation = (equation: string) => {
+    equation = equation.trim().replaceAll(" ", "");
+    equation = equation.toLowerCase();
+    let regex = /[2-9]/g;
+    //empty equation
+    if (!equation) {
+      setErrorMsg("none");
+      setValidEqn(false);
+      return false;
+    }
+    if (regex.test(equation)) {
+      setErrorMsg("digits from 2-9 aren't allowed");
+      setValidEqn(false);
+      return false;
+    }
+    //unclosed bracket
+    let stack: string[] = [];
+    for (let i = 0; i < equation.length; i++) {
+      const token = equation[i];
+      if (token == "(") stack.push(token);
+      if (token == ")") {
+        try {
+          stack.pop();
+        } catch (error) {
+          setErrorMsg("Improper parenthesses");
+          setValidEqn(false);
+          return false;
+        }
+      }
+    }
+    if (stack.length) {
+      setErrorMsg("Improper parenthesses");
+      setValidEqn(false);
+      return false;
+    }
+    regex=/[a-z]/g
+    if (!regex.test(equation)) {
+      setErrorMsg("no characters in equation");
+      setValidEqn(false);
+      return false;
+    }
+    setErrorMsg("none")
+    setValidEqn(true)
+    return true
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <p>Enter Logical Expression:</p>
+        <p
+          className={
+            errorMsg == "none" ? "hidden-paragraph" : "error-paragraph"
+          }
         >
-          Learn React
-        </a>
+          {errorMsg}
+        </p>
+        <input
+          type="text"
+          spellCheck="false"
+          onChange={(e) => {
+            validateEquation(e.target.value);
+            setEquation(e.target.value);
+          }}
+        />
+        {validEquation ? <TruthTable equation={equation} /> : <></>}
       </header>
     </div>
   );
